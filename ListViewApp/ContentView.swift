01527f92331197f6e7c1d19b792fe146a31ff82f
Shu1-net <shuichi.ama@gmail.com>
2020-09-06 15:58:12 +0900

    Initial Commit

---
+++ ListViewApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListViewApp
+//
+//  Created by 天野修一 on 2020/09/06.
+//  Copyright © 2020 shuichiama.com. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
